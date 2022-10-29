c893570c99b755810fc8afc16f298397b20a80df
Vitoslavanda <vitka.10165@gmail.com>
2022-10-29 22:57:12 +0300

    Юра лох

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 
+
+
 function App() {
   return (
     <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +12,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
